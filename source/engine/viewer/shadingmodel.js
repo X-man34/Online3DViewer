e288251e6b41b10bd3b7cf465cd4980e013b8ecd
X-man34 <caleb.hottes@gmail.com>
2026-05-06 09:44:42 -0600

    added better lights

--- source/engine/viewer/shadingmodel.js
+++ source/engine/viewer/shadingmodel.js
@@ -45,13 +45,21 @@ export class ShadingModel
 
         this.type = ShadingType.Phong;
         this.projectionMode = ProjectionMode.Perspective;
-        this.ambientLight = new THREE.AmbientLight (0x888888, 1.0 * Math.PI);
-        this.directionalLight = new THREE.DirectionalLight (0x888888, 1.0 * Math.PI);
+        this.ambientLight = new THREE.AmbientLight (0xffffff, 0.65 * Math.PI);
+        this.hemisphereLight = new THREE.HemisphereLight (0xffffff, 0x888888, 0.45 * Math.PI);
+        this.directionalLight = new THREE.DirectionalLight (0xffffff, 1.2 * Math.PI);
+        this.fillLight = new THREE.DirectionalLight (0xffffff, 0.35 * Math.PI);
+        this.lowerFillLight = new THREE.DirectionalLight (0xffffff, 0.45 * Math.PI);
+        this.sideFillLight = new THREE.DirectionalLight (0xffffff, 0.3 * Math.PI);
         this.environmentSettings = new EnvironmentSettings (null, false);
         this.environment = null;
 
         this.scene.add (this.ambientLight);
+        this.scene.add (this.hemisphereLight);
         this.scene.add (this.directionalLight);
+        this.scene.add (this.fillLight);
+        this.scene.add (this.lowerFillLight);
+        this.scene.add (this.sideFillLight);
     }
 
     SetShadingType (type)
@@ -69,12 +77,24 @@ export class ShadingModel
     UpdateShading ()
     {
         if (this.type === ShadingType.Phong) {
-            this.ambientLight.color.set (0x888888);
-            this.directionalLight.color.set (0x888888);
+            this.ambientLight.color.set (0xffffff);
+            this.ambientLight.intensity = 0.65 * Math.PI;
+            this.hemisphereLight.intensity = 0.45 * Math.PI;
+            this.directionalLight.color.set (0xffffff);
+            this.directionalLight.intensity = 1.2 * Math.PI;
+            this.fillLight.intensity = 0.35 * Math.PI;
+            this.lowerFillLight.intensity = 0.45 * Math.PI;
+            this.sideFillLight.intensity = 0.3 * Math.PI;
             this.scene.environment = null;
         } else if (this.type === ShadingType.Physical) {
-            this.ambientLight.color.set (0x000000);
-            this.directionalLight.color.set (0x555555);
+            this.ambientLight.color.set (0xffffff);
+            this.ambientLight.intensity = 0.35 * Math.PI;
+            this.hemisphereLight.intensity = 0.25 * Math.PI;
+            this.directionalLight.color.set (0xffffff);
+            this.directionalLight.intensity = 1.1 * Math.PI;
+            this.fillLight.intensity = 0.45 * Math.PI;
+            this.lowerFillLight.intensity = 0.55 * Math.PI;
+            this.sideFillLight.intensity = 0.35 * Math.PI;
             this.scene.environment = this.environment;
         }
         if (this.environmentSettings.backgroundIsEnvMap && this.projectionMode === ProjectionMode.Perspective) {
@@ -98,5 +118,22 @@ export class ShadingModel
     {
         const lightDir = SubCoord3D (camera.eye, camera.center);
         this.directionalLight.position.set (lightDir.x, lightDir.y, lightDir.z);
+
+        const viewDirection = new THREE.Vector3 (lightDir.x, lightDir.y, lightDir.z).normalize ();
+        const upDirection = new THREE.Vector3 (camera.up.x, camera.up.y, camera.up.z).normalize ();
+        const sideDirection = new THREE.Vector3 ().crossVectors (viewDirection, upDirection).normalize ();
+        const fillDirection = viewDirection.multiplyScalar (0.45).add (sideDirection.multiplyScalar (0.8)).add (upDirection.multiplyScalar (0.35));
+        this.fillLight.position.copy (fillDirection);
+
+        const lowerFillDirection = new THREE.Vector3 (lightDir.x, lightDir.y, lightDir.z).normalize ()
+            .multiplyScalar (0.35)
+            .add (new THREE.Vector3 (camera.up.x, camera.up.y, camera.up.z).normalize ().multiplyScalar (-0.9));
+        this.lowerFillLight.position.copy (lowerFillDirection);
+
+        const sideFillDirection = new THREE.Vector3 ().crossVectors (
+            new THREE.Vector3 (lightDir.x, lightDir.y, lightDir.z).normalize (),
+            new THREE.Vector3 (camera.up.x, camera.up.y, camera.up.z).normalize ()
+        ).normalize ().multiplyScalar (-1.0);
+        this.sideFillLight.position.copy (sideFillDirection);
     }
 }
